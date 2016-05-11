e073c8259a1584999d731e315fd547bdb6996be7
kisaayano <kisa.ayano@icloud.com>
2016-05-10 21:18:42 -0400

    Motor Shield code

--- vexmotor/vexmotor.ino
+++ vexmotor/vexmotor.ino
@@ -1,9 +1,11 @@
+#include <AFMotor.h>
+AF_DCMotor motor(1, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600); // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+  motor.setSpeed(255); // set the speed to 200/255
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  motor.run(FORWARD); // turn it on going forward
 
 }
